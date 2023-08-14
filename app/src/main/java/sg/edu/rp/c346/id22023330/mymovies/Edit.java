2d9af1700c50c4ffd5da0444752adf179c218438
22023330 <22023330@myrp.edu.sg>
2023-08-14 14:37:11 +0800

    L12 moreMovies

--- app/src/main/java/sg/edu/rp/c346/id22023330/mymovies/Edit.java
+++ app/src/main/java/sg/edu/rp/c346/id22023330/mymovies/Edit.java
@@ -1,5 +1,6 @@
 package sg.edu.rp.c346.id22023330.mymovies;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Edit extends AppCompatActivity {
@@ -55,16 +57,44 @@ public class Edit extends AppCompatActivity {
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DBHelper db = new DBHelper(Edit.this);
-                db.deleteMovie(data.getId());
-                finish();
+                AlertDialog.Builder myBuilder=new AlertDialog.Builder(Edit.this);
+
+                myBuilder.setTitle("Danger");
+                myBuilder.setMessage("Are you sure you want to delete the movie "+data.getTitle());
+                myBuilder.setPositiveButton("CANCEL", null);
+
+                myBuilder.setNegativeButton("DELETE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        DBHelper db = new DBHelper(Edit.this);
+                        db.deleteMovie(data.getId());
+                        finish();
+                    }
+                });
+
+                AlertDialog myDialog=myBuilder.create();
+                myDialog.show();
             }
         });
 
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                AlertDialog.Builder myBuilder=new AlertDialog.Builder(Edit.this);
+
+                myBuilder.setTitle("Danger");
+                myBuilder.setMessage("Are you sure you want to discard the changes");
+                myBuilder.setPositiveButton("DO NOT DISCARD", null);
+
+                myBuilder.setNegativeButton("DISCARD", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+
+                AlertDialog myDialog=myBuilder.create();
+                myDialog.show();
             }
         });
 
